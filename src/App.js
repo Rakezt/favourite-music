38741b7f002a3bd0a3587f020df23dd0e471d7a8
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2024-04-02 11:27:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./App.css";
+import { topSongs } from "./data";
+
+export default function App() {
+  const [select, setSelect] = useState("pop");
+
+  function changeHandler(item) {
+    setSelect(item);
+  }
+
+  return (
+    <div className="App">
+      <div className="cover">
+        <div className="nav">
+          <h1>🤘 FAVOURITE SONG</h1>
+          <h2>Select a genre to find out</h2>
+          <div>
+            {Object.keys(topSongs).map((item) => (
+              <button
+                key={item}
+                onClick={() => changeHandler(item)}
+                style={{
+                  background: select === item ? "#3498db" : "#2c3e50",
+                }}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="container container-center">
+          <ol>
+            {topSongs[select].map((list, index) => (
+              <li key={index}>
+                <div>
+                  <span className="artist">Artist</span>: {list.artist}
+                </div>
+                <div>
+                  <span className="song">Song</span>: {list.song}
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+        <div className="footer">
+          <p>Created with love by yoursweetnightmare</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const topSongs = {
+  pop: [
+    { artist: "Imagine Dragon", song: "Bad Liar" },
+    { artist: "Ed Sheeran", song: "Perfect" },
+    { artist: "Ellie Goulding", song: "Sixteen" },
+    { artist: "Ed Sheeran", song: "Beautiful People" },
+    { artist: "Sigrid", song: "Sucker Punch" },
+  ],
+  rock: [
+    { artist: "Nirvana", song: "Smells like teen Spirit" },
+    { artist: "Scorpions", song: "Rock You Like A Hurricane" },
+    { artist: "Atreyu", song: "Super Hero" },
+    { artist: "A7X", song: "Sieze the Day" },
+    { artist: "Led Zeplin", song: "Immigrant Song" },
+  ],
+  punk: [
+    { artist: "The Maine", song: "Sticky" },
+    { artist: "The Plot in You", song: "Feel Nothing" },
+    { artist: "The Maine", song: "English Girls" },
+    { artist: "The Plot in You", song: "Disposable fix" },
+    { artist: "Paramore", song: "Ain't it fun" },
+  ],
+  indie: [
+    { artist: "The 1975", song: "Robbers" },
+    { artist: "The Neighbourhood", song: "Sweather Weather" },
+    { artist: "The 1975", song: "Somebody Else" },
+    { artist: "Foster the people", song: "Pumped u kicks" },
+    { artist: "Angus & Julia Stone", song: "Chateau" },
+  ],
+};
